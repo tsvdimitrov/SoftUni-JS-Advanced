568ad579c8d9a7c563a0ddf22719220ea50e04fc
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-09-17 20:59:07 +0300

    Creating 1. Fruit.js

---
+++ 01. JS-Advanced-Syntax-Functions-and-Statements/Lab/1. Fruit.js
@@ -0,0 +1,10 @@
+function fruit(fruit, weight, money) {
+
+    let weightInKgs = weight / 1000;
+    let moneyNeeded = weightInKgs * money;
+
+
+    console.log(`I need $${moneyNeeded.toFixed(2)} to buy ${weightInKgs.toFixed(2)} kilograms ${fruit}.`)
+
+}
+fruit('orange', 2500, 1.80);
