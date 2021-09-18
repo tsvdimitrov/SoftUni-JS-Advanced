33323db2b20dc1cb483246fa97fcd3db8811fa88
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-09-18 12:12:52 +0300

    Create 4. Circle Area.js

---
+++ 01. JS-Advanced-Syntax-Functions-and-Statements/Lab/4. Circle Area.js
@@ -0,0 +1,8 @@
+function circleArea(input) {
+    if (typeof (input) !== 'number') {
+        console.log(`We can not calculate the circle area, because we receive a ${typeof (input)}.`);
+    } else {
+        console.log((Math.PI * Math.pow(input, 2)).toFixed(2));
+    }
+}
+circleArea(5);
