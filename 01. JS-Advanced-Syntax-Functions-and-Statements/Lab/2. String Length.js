3d41ff48d29773bda28ad72939f1c9cb1d480e1e
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-09-18 11:59:45 +0300

    Update 2. String Length.js

--- 01. JS-Advanced-Syntax-Functions-and-Statements/Lab/2. String Length.js
+++ 01. JS-Advanced-Syntax-Functions-and-Statements/Lab/2. String Length.js
@@ -1,4 +1,5 @@
 function stringLength(firstArgument, secondArgument, thirdArgument) {
+    
     let firstArgumentLength = firstArgument.length;
     let secondArgumentLength = secondArgument.length;
     let thirdArgumentLength = thirdArgument.length;
@@ -8,5 +9,4 @@ function stringLength(firstArgument, secondArgument, thirdArgument) {
     console.log(sumLength);
     console.log(Math.floor(sumLength / 3));
 }
-
 stringLength('chocolate', 'ice cream', 'cake');
