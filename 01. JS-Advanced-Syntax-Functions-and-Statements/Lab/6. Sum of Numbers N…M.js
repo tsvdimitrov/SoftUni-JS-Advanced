a465a871dd7e65bd632616938dd356e8ceca1319
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-09-18 12:24:42 +0300

    Create 6. Sum of Numbers N…M.js

---
+++ 01. JS-Advanced-Syntax-Functions-and-Statements/Lab/6. Sum of Numbers N…M.js
@@ -0,0 +1,13 @@
+function sumOfNumbersNM(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    let result = 0;
+    for (let i = num1; i <= num2; i++) {
+        result += i;
+    }
+    return result;
+
+}
+
+console.log(sumOfNumbersNM('1', '5'));
