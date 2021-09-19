3b0677615dc2307c89a2cbbff2a44cf4253e8166
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-09-19 13:59:58 +0300

    Update 01. Fruit.js

--- 01. Syntax-Functions-and-Statements/Exercise/01. Fruit.js
+++ 01. Syntax-Functions-and-Statements/Exercise/01. Fruit.js
@@ -3,6 +3,6 @@ function fruit(fruit, weight, money) {
     let weightInKgs = weight / 1000;
     let moneyNeeded = weightInKgs * money;
 
-    console.log(`I need $${moneyNeeded.toFixed(2)} to buy ${weightInKgs.toFixed(2)} kilograms ${fruit}.`)
+    console.log(`I need $${moneyNeeded.toFixed(2)} to buy ${weightInKgs.toFixed(2)} kilograms ${fruit}.`);
 }
 fruit('orange', 2500, 1.80);
