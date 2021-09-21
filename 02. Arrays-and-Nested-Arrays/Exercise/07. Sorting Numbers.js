1e42079e8affce9d8ebbc810303ed734dd41a59a
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-09-21 17:13:01 +0300

    Update 07. Sorting Numbers.js

---
+++ 02. Arrays-and-Nested-Arrays/Exercise/07. Sorting Numbers.js
@@ -0,0 +1,13 @@
+function sortingNumbers(input) {
+
+    input.sort((a, b) => a - b);
+    let result = [];
+
+    while (input.length) {
+        result.push(input.shift());
+        result.push(input.pop());
+    }
+
+    return result;
+}
+console.log(sortingNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
