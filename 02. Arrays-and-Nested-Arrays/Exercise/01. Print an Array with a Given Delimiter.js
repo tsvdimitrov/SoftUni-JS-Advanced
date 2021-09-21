6752a16b7c9c86638a3dfaf095b2a4b80186ac6d
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-09-21 14:45:34 +0300

    Create 01. Print an Array with a Given Delimiter.js

---
+++ 02. Arrays-and-Nested-Arrays/Exercise/01. Print an Array with a Given Delimiter.js
@@ -0,0 +1,10 @@
+function printArrWithDelimeter(arr, delimeter) {
+
+    return arr.join(delimeter);
+}
+console.log(printArrWithDelimeter(['One',
+    'Two',
+    'Three',
+    'Four',
+    'Five'],
+    '-'));
