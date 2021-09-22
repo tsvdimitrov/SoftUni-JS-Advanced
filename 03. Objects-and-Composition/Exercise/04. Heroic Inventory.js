27b309b8ae108752b1352166685400ea55e33427
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-09-22 20:14:25 +0300

    Create 04. Heroic Inventory.js

---
+++ 03. Objects-and-Composition/Exercise/04. Heroic Inventory.js
@@ -0,0 +1,19 @@
+function heroicInventory(input) {
+
+    let result = [];
+
+    while (input.length) {
+        let hero = input.shift("");
+
+        let [name, level, itemsString] = hero.split(" / ");
+        level = Number(level);
+        const items = itemsString ? itemsString.split(", ") : [];
+
+        result.push({ name, level, items });
+    }
+
+    return JSON.stringify(result);
+}
+console.log(heroicInventory(['Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara']));
