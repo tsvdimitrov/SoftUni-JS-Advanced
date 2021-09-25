9b4db8a8bc66c111775a1b85d7a7ebedd37b2c84
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-09-25 20:48:01 +0300

    Create ShowMore.js and html

---
+++ 04. DOM-Introduction/Lab/04. Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+
+    const element = document.getElementById('text');
+    element.style.display = 'inline';
+    
+    document.getElementById('more').style.display = 'none';
+}
