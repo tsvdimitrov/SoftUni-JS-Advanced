015055f51b5cf30981964c1c08a416cf1d490510
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-09-26 21:17:11 +0300

    Create 02. Pascal or Camel Case

---
+++ 04. DOM-Introduction/Exercise/02. Pascal or Camel Case/solution.js
@@ -0,0 +1,36 @@
+function solve() {
+
+  let input = document.getElementById('text').value;
+  let currentCase = document.getElementById(`naming-convention`).value;
+  let result = document.getElementById(`result`);
+
+  let selector = {
+    "Camel Case"(str) {
+      str = str.split(/\s/g).reduce((acc, el, loc) => {
+        if (loc === 0) {
+          acc += el.toLowerCase();
+        } else {
+          acc += el[0].toUpperCase();
+          acc += el.slice(1, el.length).toLowerCase();
+        }
+        return acc;
+      }, "");
+      return str;
+    },
+    "Pascal Case"(str) {
+      str = str.split(/\s/g).reduce((acc, el) => {
+        acc += el[0].toUpperCase();
+        acc += el.slice(1, el.length).toLowerCase();
+
+        return acc;
+      }, "");
+      return str;
+    },
+  };
+
+  if (selector.hasOwnProperty(currentCase)) {
+    result.innerText = selector[currentCase](input);
+  } else {
+    result.innerText = "Error!";
+  }
+}
