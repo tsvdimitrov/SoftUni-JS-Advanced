edcc97ef6ddac3c169fc0e2d4cd00d0ddcbeea0d
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-09-26 21:22:20 +0300

    Create 03. Accordion

---
+++ 04. DOM-Introduction/Exercise/03. Accordion/accordion.js
@@ -0,0 +1,8 @@
+function toggle() {
+    
+    let button = document.querySelector('.button');
+    let divExtra = document.querySelector('#extra');
+
+    divExtra.style.display = divExtra.style.display == 'none' || divExtra.style.display == '' ? 'block' : 'none';
+    button.textContent = button.textContent == 'More' ? 'Less' : 'More';
+}
