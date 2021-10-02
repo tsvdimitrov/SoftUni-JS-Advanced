a1dcb57ca20f87bfbe45fdb98647b1902c64d423
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-10-02 11:43:12 +0300

    Create 01. Sections

---
+++ 05. DOM-Manipulations-and-Events/Exercise/01. Sections/script.js
@@ -0,0 +1,22 @@
+function create(words) {
+
+   const content = document.getElementById('content');
+
+   for (let word of words) {
+
+      const div = document.createElement('div');
+      const para = document.createElement('p');
+      para.textContent = words;
+      para.style.display = 'none';
+      div.appendChild(para);
+
+      div.addEventListener('click', reveal);
+
+      content.appendChild(div);
+   }
+   
+   function reveal(e) {
+      e.currentTarget.children[0].style.display = '';
+   }
+}
+
