89a56532874bb8283e0342c3a96541d110be3bcf
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-10-02 19:29:10 +0300

    Create 03. Locked-Profile

---
+++ 05. DOM-Manipulations-and-Events/Exercise/03. Locked-Profile/app.js
@@ -0,0 +1,25 @@
+function lockedProfile() {
+
+    Array.from(document.querySelectorAll('.profile button')).forEach(b => b.addEventListener('click', onToggle));
+
+    function onToggle(e) {
+        const profile = e.target.parentElement;
+        const isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+
+        if (isActive) {
+            const infoDif = profile.querySelector('div');
+
+            if (e.target.textContent == 'Show more') {
+                infoDif.style.display = 'block';
+                e.target.textContent = 'Hide it';
+            } else {
+                infoDif.style.display = 'block';
+                e.target.textContent = 'Show more';
+            }
+        }
+    }
+}
+
+
+
+
