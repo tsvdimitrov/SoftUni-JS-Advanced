7c6671eb0eb2d2e754daf917e9f37fb5f16f7ace
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-10-03 08:57:58 +0300

    Create 04. Full-Dropdown

---
+++ 05. DOM-Manipulations-and-Events/Exercise/04. Fill-Dropdown/dropdown.js
@@ -0,0 +1,13 @@
+function addItem() {
+    
+    const menu = document.getElementById('menu');
+    const text = document.getElementById('newItemText');
+    const value = document.getElementById('newItemValue');
+
+    let option = document.createElement('option');
+    option.textContent = text.value;
+    option.value = value.value;
+    menu.appendChild(option);
+    text.value = '';
+    value.value = '';
+}
