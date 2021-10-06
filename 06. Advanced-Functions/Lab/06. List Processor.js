61bc92e275b1f4e80e42b68510aeb732c3e40481
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-10-06 20:48:06 +0300

    Create 06. List Processor.js

---
+++ 06. Advanced-Functions/Lab/06. List Processor.js
@@ -0,0 +1,23 @@
+function listProcessor(arr) {
+
+    let modify = (function () {
+        let result = []
+        return {
+            add(str) {
+                result.push(str)
+            },
+            remove(str) {
+                while (result.includes(str)) result.splice(result.indexOf(str), 1)
+            },
+            print() {
+                console.log(result.join(','))
+            }
+        }
+    })()
+    arr.forEach(line => {
+        let [command, str] = line.split(' ')
+        modify[command](str)
+    })
+}
+
+listProcessor(['add pesho', 'add george', 'add peter', 'remove peter','print']);
