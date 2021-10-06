45adf5d07a1a59fdee2fa090b91793d7c9c8e046
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-10-06 20:51:37 +0300

    Create 07. Cars.js

---
+++ 06. Advanced-Functions/Lab/07. Cars.js
@@ -0,0 +1,33 @@
+function cars(input) {
+
+    let cars = {};
+    let commandParser = {
+        create(carName, inherits, parentName) {
+            (cars[carName] = inherits ? Object.create(cars[parentName]) : {});
+        },
+        set(carName, key, value) {
+            cars[carName][key] = value;
+        },
+        print(carName) {
+            let output = [];
+            for (let element in cars[carName]) {
+                output.push(`${element}:${cars[carName][element]}`);
+            }
+            console.log(output.join(', '));
+        },
+    }
+
+    input.forEach(line => {
+        let [command, ...args] = line.split(' ');
+        commandParser[command](...args);
+    });
+}
+
+cars([
+    'create c1',
+    'create c2 inherit c1',
+    'set c1 color red',
+    'set c2 model new',
+    'print c1',
+    'print c2'
+])
