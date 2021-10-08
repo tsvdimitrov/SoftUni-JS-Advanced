e0cfa315f23c8eadee0656ae633e5056e72ea27f
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-10-08 13:34:00 +0300

    Create 08. Next Article

---
+++ 06. Advanced-Functions/Exercise/08. Next Article/script.js
@@ -0,0 +1,12 @@
+function getArticleGenerator(articles) {
+
+    let contentDiv = document.querySelector('#content');
+
+    function closure() {
+        let articleTag = document.createElement('article');
+        if (articles.length <= 0) return
+        articleTag.textContent = articles.shift()
+        contentDiv.appendChild(articleTag);
+    }
+    return closure;
+}
