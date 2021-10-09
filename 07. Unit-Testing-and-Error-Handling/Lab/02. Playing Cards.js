f635a64780ebf4c204b8762ddc0bd48b5f1e2b73
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-10-09 19:10:30 +0300

    Create 02. Playing Cards.js

---
+++ 07. Unit-Testing-and-Error-Handling/Lab/02. Playing Cards.js
@@ -0,0 +1,24 @@
+function createCard(face, suit) {
+    
+    const validFaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q", "A"]
+    const suitToString = {
+        "S": "\u2660",
+        "H": "\u2665",
+        "D": "\u2666",
+        "C": "\u2663"
+    }
+    if (!validFaces.includes(face)) {
+        throw new Error('Invalid face')
+    } else if (!Object.keys(suitToString).includes(suit)) {
+        throw new Error("Invalid suit")
+    }
+    return {
+        face,
+        suit,
+        toString() {
+            return `${face}${suitToString[suit]}`
+        }
+    }
+}
+let myCard = createCard('A', 'S')
+console.log(myCard.toString())
