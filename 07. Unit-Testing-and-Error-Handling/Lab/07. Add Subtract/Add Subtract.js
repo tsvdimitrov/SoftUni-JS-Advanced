9a33cfcb141c6d51b2b46d2c3e8993aecfdb5f98
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-10-10 20:45:49 +0300

    Create 07. Add Subtract

---
+++ 07. Unit-Testing-and-Error-Handling/Lab/07. Add Subtract/Add Subtract.js
@@ -0,0 +1,10 @@
+const { expect } = require('chai');
+
+function createCalculator() {
+    let value = 0;
+    return {
+        add: function (num) { value += Number(num); },
+        subtract: function (num) { value -= Number(num); },
+        get: function () { return value; }
+    }
+}
