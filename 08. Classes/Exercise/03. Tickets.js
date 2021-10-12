b4322d666544ada21d4706896f2d5214751da700
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-10-12 20:17:26 +0300

    Create 03. Tickets.js

---
+++ 08. Classes/Exercise/03. Tickets.js
@@ -0,0 +1,35 @@
+function tickets(arr, type) {
+
+    function splitLine(line) {
+        return line.split("|");
+    }
+
+    function convertToTicket([destination, price, status]) {
+        return new Ticket(destination, Number(price), status);
+    }
+
+    class Ticket {
+        constructor(destination, price, status) {
+            this.destination = destination;
+            this.price = price;
+            this.status = status;
+        }
+    }
+
+    const sortMapper = {
+        "destination": (a, b) => a.destination.localeCompare(b.destination),
+        "price": (a, b) => a.price - b.price,
+        "status": (a, b) => a.status.localeCompare(b.status)
+    }
+
+    return arr.map(splitLine)
+        .map(convertToTicket)
+        .sort(sortMapper[type]);
+}
+
+console.log(tickets(['Philadelphia|94.20|available',
+    'New York City|95.99|available',
+    'New York City|95.99|sold',
+    'Boston|126.20|departed'],
+    'destination'
+));
