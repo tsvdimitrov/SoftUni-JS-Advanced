25b742da0665b9b51741da27c8d91d1ecb24cb46
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-10-13 20:33:05 +0300

    Update 02. String Extension.js

--- 09. Prototypes-and-Inheritance/Exercise/02. String Extension.js
+++ 09. Prototypes-and-Inheritance/Exercise/02. String Extension.js
@@ -50,9 +50,14 @@
 }
 )();
 
-str = new String();
-str = 'mystring';
-//str = str.ensureStart('my');
-str = str.ensureStart('hello');
-str = str.ensureEnd('bye');
+str = str.ensureStart('my');
+str = str.ensureStart('hello ');
+str = str.truncate(16);
 str = str.truncate(14);
+str = str.truncate(8);
+str = str.truncate(4);
+str = str.truncate(2);
+str = String.format('The {0} {1} fox',
+    'quick', 'brown');
+str = String.format('jumps {0} {1}',
+    'dog');
