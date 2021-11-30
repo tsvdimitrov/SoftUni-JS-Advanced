7899cb0a95135c52a9b263610adf457b3f0c4400
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-11-30 17:29:41 +0200

    Update SumTable.js

--- 04. DOM-Introduction/Lab/06. Sum Table/SumTable.js
+++ 04. DOM-Introduction/Lab/06. Sum Table/SumTable.js
@@ -4,8 +4,9 @@ function sumTable() {
     let sum = 0;
 
     for (let i = 1; i < rows.length - 1; i++) {
-        const cols = rows[i].children;
-        sum += Number(cols[cols.length - 1].textContent);
+        const cell = rows[i].lastElementChild;
+        sum += Number(cell.textContent);
     }
+
     document.getElementById('sum').textContent = sum;
 }
