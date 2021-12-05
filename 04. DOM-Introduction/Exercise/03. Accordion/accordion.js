753e97cc211dabf41fa939503a0bd1d7cea2632f
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-05 19:55:18 +0200

    Update accordion.js

--- 04. DOM-Introduction/Exercise/03. Accordion/accordion.js
+++ 04. DOM-Introduction/Exercise/03. Accordion/accordion.js
@@ -1,8 +1,13 @@
 function toggle() {
-    
-    let button = document.querySelector('.button');
-    let divExtra = document.querySelector('#extra');
 
-    divExtra.style.display = divExtra.style.display == 'none' || divExtra.style.display == '' ? 'block' : 'none';
-    button.textContent = button.textContent == 'More' ? 'Less' : 'More';
+    const div = document.querySelector('#extra');
+    const btn = document.getElementsByClassName('button')[0];
+
+    if (div.style.display === 'block') {
+        btn.textContent = 'More';
+        div.style.display = 'none';
+    } else {
+        btn.textContent = 'Less';
+        div.style.display = 'block';
+    }
 }
