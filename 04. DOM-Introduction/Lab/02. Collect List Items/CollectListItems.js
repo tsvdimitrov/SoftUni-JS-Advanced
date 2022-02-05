d2690e48cc658ee72620e9cca60333e8d3371fdf
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-02-05 21:23:17 +0200

    Update CollectListItems.js

--- 04. DOM-Introduction/Lab/02. Collect List Items/CollectListItems.js
+++ 04. DOM-Introduction/Lab/02. Collect List Items/CollectListItems.js
@@ -2,14 +2,11 @@ function extractText() {
 
     const items = document.getElementById('items').children;
 
-    const result = [
-
-    ];
+    const result = [];
 
     for (let item of Array.from(items)) {
         result.push(item.textContent);
     }
-    
 
     document.getElementById('result').value = result.join('\n');
 }
