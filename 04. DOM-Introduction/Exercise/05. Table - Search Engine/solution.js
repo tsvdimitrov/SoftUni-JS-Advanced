7d7fa718e3c897725b2ec49169afcb9372fd7576
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-02-06 21:26:47 +0200

    Update solution.js

--- 04. DOM-Introduction/Exercise/05. Table - Search Engine/solution.js
+++ 04. DOM-Introduction/Exercise/05. Table - Search Engine/solution.js
@@ -8,8 +8,10 @@ function solve() {
       const input = document.querySelector('#searchField').value.toLowerCase();
 
       for (let row of rows) {
+
          if (row.textContent.toLowerCase().includes(input)) {
-            row.setAttribute('class', 'select');
+            row.setAttribute('
+            class', 'select');
          } else {
             row.removeAttribute('class');
          }
