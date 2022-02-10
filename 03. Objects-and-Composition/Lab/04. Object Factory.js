1a2c33c72b0c9487963cffeae3f7fe43c353951e
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-02-10 17:01:01 +0200

    Update 04. Object Factory.js

--- 03. Objects-and-Composition/Lab/04. Object Factory.js
+++ 03. Objects-and-Composition/Lab/04. Object Factory.js
@@ -8,7 +8,7 @@ function factory(library, orders) {
 
         for (let part of order.parts) {
             result[part] = library[part];
-            console.log(librarydsdas[part]);
+            console.log(library[part]);
         }
 
         return result;
@@ -21,8 +21,8 @@ const library = {
     },
     scan: function () {
         console.log(`${this.name} is scanning a document`);
-    },dsa
-    play: function (artist, tradsadck) {
+    },
+    play: function (artist, track) {
         console.log(`${this.name} is playing '${track}' by ${artist}`);
     },
 };
@@ -30,7 +30,7 @@ const library = {
 const orders = [
     {
         template: { name: 'ACME Printer' },
-        parts: ['print']sa
+        parts: ['print']
     },
     {
         template: { name: 'Initech Scanner' },
